adcf2189cb0e6d76ffaec65c22417b4d2a43a8d2
ElizabethMV13 <dulce.mendoza@ingenieria.unam.edu>
2023-05-27 22:48:37 -0600

    Cambios

--- app/src/main/java/com/example/ejercicio2/SegundoFragment.kt
+++ app/src/main/java/com/example/ejercicio2/SegundoFragment.kt
@@ -1,12 +1,16 @@
 package com.example.ejercicio2
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.example.ejercicio2.model.EstudianteViewHolder
 import com.example.ejercicio2.model.StudentAdapter
 import com.example.ejercicio2.model.Student
 import com.example.ejercicio2.model.StudentApiService
@@ -26,8 +30,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SegundoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class SegundoFragment : Fragment() {
 
+    private val studentList: MutableList<Student> = mutableListOf()
+    lateinit var adapterStudent: StudentAdapter
+    lateinit var recyclerView: RecyclerView
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -47,34 +55,54 @@ class SegundoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val retrofit= Retrofit.Builder()
+        val view = inflater.inflate(R.layout.fragment_segundo, container, false)
+
+
+        // Configurar el RecyclerView
+        recyclerView = view.findViewById(R.id.recyclerViewEstudent)
+
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        fetchStudentData()
+
+        return view
+    }
+
+    private fun fetchStudentData() {
+
+        // Crear una instancia de Retrofit
+        val retrofit = Retrofit.Builder()
             .baseUrl("https://private-b21a12-ejercicio2estudiantes.apiary-mock.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+            .create(StudentApiService::class.java)
 
-        val api = retrofit.create(StudentApiService::class.java)
-        api.getStudents().enqueue(object : Callback<List<Student>>{
+
+        // Hacer la llamada a la API para obtener los datos de los estudiantes
+        var retroData = retrofit.getStudents().enqueue(object : Callback<List<Student>> {
             override fun onResponse(call: Call<List<Student>>, response: Response<List<Student>>) {
+                if (response.isSuccessful) {
+                    val students = response.body()
+                    students?.let {
+                        // Agregar los estudiantes a la lista
+                        studentList.addAll(students)
 
+                        adapterStudent = StudentAdapter(requireContext(), students )
+
+                        recyclerView.adapter = adapterStudent
+
+
+                        Log.d("data", students.toString())
+                    }
+                } else {
+                    // Manejar el caso de respuesta no exitosa
+                }
             }
 
             override fun onFailure(call: Call<List<Student>>, t: Throwable) {
-                TODO("Not yet implemented")
+                // Manejar el caso de error de la llamada
             }
-
         })
-
-
-        return inflater.inflate(R.layout.fragment_segundo, container, false)
-
-    }
-
-    private fun showData(student: List<Student>){
-        recyclerView.apply{
-            LayoutManager = LinearLayoutManager(this@fragment_segundo)
-            adapter = StudentAdapter(student)
-        }
     }
 
 
@@ -97,4 +125,7 @@ class SegundoFragment : Fragment() {
                 }
             }
     }
+
+
+
 }
--- app/src/main/java/com/example/ejercicio2/model/StudentAdapter.kt
+++ app/src/main/java/com/example/ejercicio2/model/StudentAdapter.kt
@@ -1,14 +1,16 @@
 package com.example.ejercicio2.model
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.ejercicio2.R
 
-class StudentAdapter (var con : Context, val listStudent:List<Student>):RecyclerView.Adapter<StudentAdapter.ViewHolder>() {
+class StudentAdapter (var context : Context, val listStudent:List<Student>):RecyclerView.Adapter<StudentAdapter.ViewHolder>() {
 
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
@@ -16,15 +18,24 @@ class StudentAdapter (var con : Context, val listStudent:List<Student>):Recycler
         val actor = itemView.findViewById<TextView>(R.id.item_actor)
         val img = itemView.findViewById<ImageView>(R.id.item_img)
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        var view= LayoutInflater.from(context).inflate(R.layout.item_personaje, parent,false)
+        return ViewHolder(view)
     }
-
-    override fun getItemCount(): Int = listStudent.size
-
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        Glide.with(context).load(listStudent[position].image).into(holder.img)
+
+        holder.nombre.text = listStudent[position].nombre
+        holder.actor.text = listStudent[position].actor
+
     }
+
+    override fun getItemCount(): Int {
+        return listStudent.count()
+    }
+
+
+
+
 }
